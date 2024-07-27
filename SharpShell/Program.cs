36fd1578288b15b546f50cc766ba81a2143edf88
LukeFZ <17146677+LukeFZ@users.noreply.github.com>
2024-07-27 23:38:31 +0200

    Add project files

---
+++ SharpShell/Program.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.PowerShell;
+
+namespace SharpShell
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            UnmanagedPSEntry.Start(Array.Empty<string>(), 0);
+        }
+    }
+}
